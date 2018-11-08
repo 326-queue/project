5a933a1de61af99603b6d1545ced44643969cbda
Itamar Levy-Or <ilevyor@umass.edu>
2018-11-08 13:29:14 -0500

    moved hackathons model into dashboard model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+class Hackathon(models.Model):
+    fundraising_goal = models.IntegerField()
+    total_funding_raised = models.IntegerField
+    fundraising_deadline = models.DateField()
+    hackathon_date = models.DateField()
--- hackathon/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.urls import reverse
-
-class Hackathon(models.Model):
-    fundraising_goal = models.IntegerField()
-    total_funding_raised = models.IntegerField
-    fundraising_deadline = models.DateField()
-    hackathon_date = models.DateField()
